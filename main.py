624da2c1483b23abf886507657d91668e022cf6a
igorzinoviev08 <zinovievigor08@gmail.com>
2023-07-10 23:40:08 +0300

    CW_3 add files README.md and .gitignore

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from settings import PATH_OPERATION_JSON
 from app.models.data_manager import DataManager
-from app.models.operations_manager import OperationManager
+from app.models.operations_manager import OperationsManager
 
 if __name__ == '__main__':
     all_operations = DataManager.read_from_json(PATH_OPERATION_JSON)
-    last_five_operation = OperationManager(all_operations)
+    last_five_operation = OperationsManager(all_operations)
     last_five_operation.show_last_operation()
